aaab9597c29ca9d4a5f540931acb56f1af3370a5
Kolvinkst <einbenutzer1998@gmail.com>
2024-01-22 00:27:02 +0700

    Добавьте файлы проекта.

---
+++ Registration.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace usersAppWithTalk
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (this.WindowState == System.Windows.WindowState.Maximized)
+            {
+                innerSpace.Width *= 2;
+                innerSpace.Height *= 2;
+            }
+            else if (this.WindowState == System.Windows.WindowState.Normal)
+            {
+                innerSpace.Width /= 2;
+                innerSpace.Height /= 2;
+            }
+        }
+    }
+}
